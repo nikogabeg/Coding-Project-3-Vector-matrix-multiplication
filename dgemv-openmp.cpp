7c0e693683c15ce4b30a7f95c99f232f45b0a42c
nikogabeg <ngaledo@sfsu.edu>
2025-04-25 02:55:42 -0700

    progress #2

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -14,16 +14,15 @@ const char* dgemv_desc = "OpenMP dgemv.";
 
 void my_dgemv(int n, double* A, double* x, double* y) {
 
+   // use #pragma omp parallel for to parallelize row computations from lecture page 51
    #pragma omp parallel for
    for (int i = 0; i < n; i++) {
       double temp = 0.0;
       for (int j = 0; j < n; j++) {
-          temp += A[i * n + j] * x[j];
+          temp += A[i * n + j] * x[j]; // Row-major access (Page 9)
       }
-      #pragma omp atomic
-      y[i] += temp;
+      y[i] += temp; // No atomic/critical needed (distinct per thread, Page 51)
   }
-
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you will want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
