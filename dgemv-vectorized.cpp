82d11db7174674ecd1ed34720a0123bf1383901d
nikogabeg <nikogabeg@gmail.com>
2025-04-29 02:38:22 -0700

    progress 3.0

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,14 +8,15 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
-   #pragma omp simd // compiler for vectorization from P&H Reading
+
+   #pragma omp parallel for // compiler for vectorization from P&H Reading
     for (int i = 0; i < n; i++) {
-        double temp = 0.0;
+      double temp = 0.0;
         
-        // Inner loop should vectorize
-        for (int j = 0; j < n; j++) {
-            temp += A[i * n + j] * x[j];
-        }
-        y[i] += temp;
+      // Let compiler auto-vectorize this inner loop
+      for (int j = 0; j < n; j++) {
+          temp += A[i * n + j] * x[j];
+      }
+      y[i] += temp;
     }
 }
