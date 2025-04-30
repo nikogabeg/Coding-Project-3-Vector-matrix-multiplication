62ebdc1f6d2562b7b3ebe06217cda3852f3000dc
nikogabeg <nikogabeg@gmail.com>
2025-04-29 22:07:47 -0700

    progress 4.1

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -20,7 +20,10 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // use #pragma omp parallel for to parallelize row computations from lecture page 51
    #pragma omp parallel for
    for (int i = 0; i < n; i++) {
-      double temp = 0.0;
+      double temp = y[i]; // Initialize with y[i]
+
+      // Vectorized inner loop
+      #pragma omp simd
       for (int j = 0; j < n; j++) {
           temp += A[i * n + j] * x[j]; // Row-major access (Page 9)
       }
